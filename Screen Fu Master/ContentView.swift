d0356a1f6d883b357033147e4b17786642f5ec43
Tarik <tarikmerrylees@gmail.com>
2024-05-27 09:31:17 +0100

    Initial Commit

---
+++ Screen Fu Master/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Screen Fu Master
+//
+//  Created by Tarik Merrylees on 27/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Screen Fu Master/Screen_Fu_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screen_Fu_MasterApp.swift
+//  Screen Fu Master
+//
+//  Created by Tarik Merrylees on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Screen_Fu_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
